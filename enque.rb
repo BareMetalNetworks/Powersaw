8dc689c7f3002ea35510eaa9083d77ee3ddb0134
seshin <sam@baremetalnetworks.com>
2015-08-15 12:05:32 -0700

    new file: mailer

--- enque.rb
+++ enque.rb
@@ -5,7 +5,6 @@ require 'connection_pool'
 require 'logger'
 require 'thread'
 require 'rb-inotify'
-# Cuz shes my log hooker... yeaa yeaaaaaaa... my log hooker!
 
 $options = {}
 
@@ -19,37 +18,84 @@ $DEBUG = true
 $logger = Logger.new('enque.log', 'a+')
 
 Redis::Objects.redis = ConnectionPool.new(size: 5, timeout: 5) {
-	Redis.new({host: $options[:host], port: $options[:port], db: $options[:db]})}
+	Redis.new({host: $options[:host], port: $options[:port], db: $options[:db], logger: true})}
 
-$r = Redis::List.new('system:log', :marshall => true, :expiration => 5)
+$r = Redis::List.new('system:log', :marshal => true, :expiration => 5)
+$spool = Redis::List.new('system:log:spool', :marshal => true)  # for end of day mailer
 
-$r = Redis::List.new('system:log:archive', :marshall => true)
+$archive = Redis::List.new('system:log:archive', :marshal => true)
+
+def mailer(msg, address)
+	sleep 5
+	`echo #{msg} | /usr/bin/ssmtp #{address}`
+
+end
+
+def bench(name)
+ elapsed =  Benchmark.realtime {yield}
+ return "%s: %.2fs" % [name, elapsed]
+  end
+
+def forker(brock)
+pid = fork do
+	yield
+end
+Process.detach(pid)
+end
+
+$resultFactory = lambda  {|x,y|  return "#{Time.now}: File #{x}, #{y} action(s) have been executed as a result" }
 
 
+
+def parser event
+	tim = "#{Time.now}: "
+	fil = " Filename: #{event.name} "
+	if event.flags.include? :create #, :access
+		$r << "#{tim} File created: #{fil}"
+	end
+
+	if event.flags.include? :delete
+		$r << "#{tim} File deleted: #{fil}"
+		$archive << "#{tim} #{fil}: Deleted"
+		sleep 5
+		mailer(message, "transiencymail@gmail.com")
+		sleep 5
+		mailer(message, "support@baremetalnetworks.com")
+
+	end
+
+	if event.flags.include? :modify
+		$r << "#{tim} File #{fil} : Was modified"
+		$archive << "#{tim} #{fil} : Modified"
+
+	end
+	if event.flags.include? :moved_from
+		$r << "#{tim} File #{fil} : Was modified"
+		$archive << "#{tim} #{fil} : Modified"
+
+	end
+	if event.flags.include? :access
+		$r << "#{tim} File #{fil} : Was modified"
+	end
+end
+
+
+
+begin
+
+	while true do
 
 
 #dirHookEtc = Thread.new{
 
+		end
 
  #begin
 	hook = INotify::Notifier.new
-    tim = "#{Time.now}: "
-    fil = " Filename: #{event.name} "
+		hook.watch("/etc/", :create, :delete, :modify, :access, :moved_from)
+
 		 p "Event name: #{event.name} \n Event Methods: #{event.methods.sort}"
-		 if event.flags.include? :create
-			     $r << "#{tim} File created. #{fil}"
-		 end
-
-		 if event.flags.include? :delete
-			  $r << "#{tim} File deleted. #{fil}"
-			  $archive << "#{tim} #{fil} : Deleted"
-		 end
-
-		 if event.flags.include? :modify
-			 $r << "#{tim} File #{fil} : Was modified"
-			 $archive << "#{tim} #{fil} : Modified"
-
-		 end
+	   parser(event)
 		 sleep 5
 	end
 
